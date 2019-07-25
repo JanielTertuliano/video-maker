d44cc92a8f569c6f6760c84aed902a7a4d1191cb
Janiel <janiel.web@gmail.com>
2019-07-25 14:02:21 -0300

    add watson natural language understanting service

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const robots = {
 }
 
 async function start () {
-    const content = {}
+    const content = {
+        maximumSentences: 7
+    }
 
     content.searchTerm = askAndReturnSearchTerm();
     content.prefix = askAndReturnPrefix();
@@ -29,7 +31,7 @@ async function start () {
         const selectedLangText = language[selectedLangIndex];
         return selectedLangText;
     }
-    // console.log(content);
+    console.log(JSON.stringify(content, null, 4));
 }
 
 start()
