4ee19063cb471dcc0905853c0c0645ea4cee00b5
Janiel <janiel.web@gmail.com>
2019-07-26 16:36:56 -0300

    Init convert image

--- robots/image.js
+++ robots/image.js
@@ -2,6 +2,7 @@ const imageDownload = require('image-downloader');
 const state = require('./state.js');
 const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
+const gm = require('gm').subClass({imageMagick: true});
 
 const googleSearchCredentials = require('../credentials/google-search.json');
 
@@ -10,8 +11,9 @@ async function robot() {
 
     await fetchImagesOgAllSentences(content);
     await downloadAllImages(content);
+    await convertAllImages(content);
 
-    state.save(content);
+    // state.save(content);
 
     async function fetchImagesOgAllSentences(content) {
         for (const sentence of content.sentences) {
@@ -69,6 +71,52 @@ async function robot() {
             dest: `./content/${fileName}`
         })
     }
+
+    async function convertAllImages(content) {
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex ++) {
+            await convertImage(sentenceIndex);
+        }
+    }
+
+    async function convertImage(sentenceIndex) {
+        return new Promise((resolve, reject) => {
+          const inputFile = `./content/${sentenceIndex}-original.png[0]`;
+          const outputFile = `./content/${sentenceIndex}-converted.png`;
+          const width = 1920;
+          const height = 1080;
+    
+          gm()
+            .in(inputFile)
+            .out('(')
+              .out('-clone')
+              .out('0')
+              .out('-background', 'white')
+              .out('-blur', '0x9')
+              .out('-resize', `${width}x${height}^`)
+            .out(')')
+            .out('(')
+              .out('-clone')
+              .out('0')
+              .out('-background', 'white')
+              .out('-resize', `${width}x${height}`)
+            .out(')')
+            .out('-delete', '0')
+            .out('-gravity', 'center')
+            .out('-compose', 'over')
+            .out('-composite')
+            .out('-extent', `${width}x${height}`)
+            .write(outputFile, (error) => {
+              if (error) {
+                return reject(error);
+              }
+    
+              console.log(`> [video-robot] Image converted: ${outputFile}`);
+              resolve();
+            });
+    
+        });
+    }
+    
 
 }
 
